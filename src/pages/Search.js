d85f9a86249cbbd0a9c344ee3597e2321292ffd6
michael 2 <trujillom9@gmail.com>
2020-12-02 13:01:31 -0800

    second attempt uploading homework assignment #19

---
+++ src/pages/Search.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Search extends Component {
+  state = {
+    search: "",
+    breeds: [],
+    results: [],
+    error: ""
+  };
+
+  
+}
+
+export default Search;
---
+++ src/utils/API.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+// code taken from an activity in need of refactor
+
+// export default {
+//   getRandomDog: function() {
+//     return axios.get("https://dog.ceo/api/breeds/image/random");
+//   },
+//   getDogsOfBreed: function(breed) {
+//     return axios.get("https://dog.ceo/api/breed/" + breed + "/images");
+//   },
+//   getBaseBreedsList: function() {
+//     return axios.get("https://dog.ceo/api/breeds/list");
+//   }
+// };
